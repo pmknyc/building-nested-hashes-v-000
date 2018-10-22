ea9586c64dde6c1585ba97767080a5ac8d71cf9b
Paula McKinley <pmknyc@optimum.net>
2018-10-22 20:33:16 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -14,22 +14,22 @@ def third_challenge
         :age => "15",
         :status => "alive"
         },
-
-      ]
    },
    :capulet => {
-      :patriarch => {
-        #your key/value pairs here
-        },
+     :patriarch => {
+     :name => "Lord Capulet",
+     :age => "50"
+    },
       :matriarch => {
-        #your key/value pairs here
-        },
+        :name => "Laty Capulet",
+        :age => "51"
+    },
       :heroine => {
-        #your key/value pairs here
-        },
-      :heroine_friends => []
-   }
-  }
+        :name => "Juliet",
+        :age => "15",
+        :status => "alive"
+    },
+}
 
 
 end
